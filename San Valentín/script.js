0f478ac792f74d28f31d981da139325ccd59de25
Andre07-hub <159399568+Andre07-hub@users.noreply.github.com>
2024-02-07 21:54:35 -0500

    Add files via upload

---
+++ San Valentín/script.js
@@ -0,0 +1,7 @@
+document.getElementById('btnSi').addEventListener('click', function() {
+    alert('¡Feliz San Valentín! ❤️');
+});
+
+document.getElementById('btnNo').addEventListener('click', function() {
+    alert('Quizás la próxima vez. 😊');
+});
